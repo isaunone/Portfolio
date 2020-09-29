7469685b5aba7e459c4b092ea943ab50f593707f
isaunone <valerievarias@gmail.com>
2020-09-29 16:57:49 +0200

    test setup

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-echo 'www.valerievarias.com' > CNAME
+#echo 'www.valerievarias.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:isaunone/portfolio-site.git master:gh-pages
+git push -f git@github.com:isaunone/test-site.git master:gh-pages
 cd -
